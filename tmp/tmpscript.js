55e40f859d8d1553a2947b508ba618bca0619abb
YIMSEBIN <jij09123@gmail.com>
2023-11-07 21:30:17 +0900

    refactor : 폴더 리팩토링 \n resolve : #1

---
+++ tmp/tmpscript.js
@@ -0,0 +1,7 @@
+const showImageBtn = document.getElementById("showImageBtn");
+const imageContainer = document.getElementById("imageContainer");
+
+showImageBtn.addEventListener("click", function() {
+    // 이미지 컨테이너를 표시하도록 스타일 변경
+    imageContainer.style.display = "block";
+});
